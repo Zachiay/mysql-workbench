c3a0de77831e621935fe4cfb83b3f6c8b3c7e5c9
Marcin Marek <marcin.marek@oracle.com>
2024-04-10 16:55:35 +0200

    fix python files build after license upgrade

--- library/python/workbench/change_tracker.py
+++ library/python/workbench/change_tracker.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013, 2019, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2013, 2024, Oracle and/or its affiliates. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2.0,
@@ -10,7 +10,7 @@
 # documentation.  The authors of MySQL hereby grant you an additional
 # permission to link the program and your derivative works with the
 # separately licensed software that they have either included with
- * the program or referenced in the documentation.
+# the program or referenced in the documentation.
 # This program is distributed in the hope that it will be useful,  but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
