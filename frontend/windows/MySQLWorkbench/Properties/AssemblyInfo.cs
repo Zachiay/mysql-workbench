f921607eb4791a26ec91b1558d7f5fe4f5c54eb4
Marcin Marek <marcin.marek@oracle.com>
2024-06-03 10:57:57 +0200

    set version numer to 8.0.38

--- frontend/windows/MySQLWorkbench/Properties/AssemblyInfo.cs
+++ frontend/windows/MySQLWorkbench/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using MySQL.GUI.Workbench;
 //      Minor Version 
 //      Release Number
 //
-[assembly: AssemblyVersion("8.0.37")]
-[assembly: AssemblyFileVersion("8.0.37")]
+[assembly: AssemblyVersion("8.0.38")]
+[assembly: AssemblyFileVersion("8.0.38")]
